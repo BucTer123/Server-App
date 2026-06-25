f41d14cfe81e222195b76be5d2317cee8367a8bb
SHALAWA <bambukvova3@gmail.com>
2026-06-25 17:03:24 +0300

    Create second_file.c

---
+++ App(GTK + PythonFlask)/second_file.c
@@ -0,0 +1,14 @@
+#include <gtk/gtk.h>
+#include "second_file.h"
+
+void about_window() {
+  
+  GtkWidget *window2 = gtk_new_window(GTK_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(window2), 800, 600);
+  gtk_window_set_title(GTK_WINDOW(window2), "About Window");
+
+  GtkWidget *text_about = gtk_new_label("This window is empty ");
+  g_container_add(GTK_CONTAINER(window2), text_about);
+
+  return 0;
+}
